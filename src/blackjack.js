6a54358f026eaabe245034b545f73dd82ba688de
avis <b3326@oic.jp>
2017-08-04 07:30:20 -0700

    ユーザー入力

--- src/blackjack.js
+++ src/blackjack.js
@@ -1,20 +1,10 @@
 const Trump = require('./trump.js');
+const ReadLineSync = require('readline-sync');
+
 class BlackJack{
     constructor(){
-        console.log(1);
-        let trump = new Trump(Trump.MARK.D, 1);
-        let allTump = [];
-
-        for (let m of Trump.ALL_MARK) {    
-            allTump.push(... new Array(13)
-                .fill(0)
-                .map((_, i) => i + 1)
-                .map(v => new Trump(m, v))
-            )
-        }
-
-
-        console.log(trump, allTrump);
+        this.deck = this.getAllCards();
+        this.main();
     }
     getAllCards(){
         let allTrump = [];
@@ -27,6 +17,10 @@ class BlackJack{
             )
         }
         return allTrump
+    }
+    main(){
+        let userInput = ReadLineSync.question('user:');
+        console.log(`userInput : ${userInput}`);
     }
 }
 module.exports = BlackJack;
