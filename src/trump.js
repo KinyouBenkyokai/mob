d8d16997d6ab214a1d84d0daa1d12bda55e6a73e
kamijin_fanta <kamijin@live.jp>
2017-08-04 22:56:34 +0900

    trumpのmarkをgetterにした

--- src/trump.js
+++ src/trump.js
@@ -1,9 +1,11 @@
 class Trump{
-    static MARK = {
-        D : 1,
-        C : 2,
-        S : 3,
-        H : 4
+    static get MARK () {
+        return {
+            D : 1,
+            C : 2,
+            S : 3,
+            H : 4
+        }
     };
     constructor(mark, number){
         this.mark = mark;
