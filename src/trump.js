6ca1db574be8232dc998ce4fe29027c2e9f5162c
avis <b3326@oic.jp>
2017-08-04 06:44:39 -0700

    マーク追加

--- src/trump.js
+++ src/trump.js
@@ -1,4 +1,10 @@
 class Trump{
+    static MARK = {
+        D : 1,
+        C : 2,
+        S : 3,
+        H : 4
+    };
     constructor(mark, number){
         this.mark = mark;
         this.number = number;
--- test/trump-test.js
+++ test/trump-test.js
@@ -1,3 +1,18 @@
+const chai = require('chai'),
+      should = chai.should(),
+      assert = require('assert');
 const Trump = require('../src/trump.js');
 
+describe('trump',()=>{
+    let trump = new Trump("clover",1);
+    it('１＋１＝２となるテスト',()=>{
+        assert(1+1==2);
+    });
+    it('trumpを生成できる',()=>{
+        trump.should.have.property('number',1);
+    });
+    it('staticなマークを取得できる',()=>{
+        assert( Trump.MARK.H == 4 );
+    });
+});
 console.log(1);
