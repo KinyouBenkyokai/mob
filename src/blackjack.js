3e1e727d1b2a98af907e8238eb910428861d5d7a
kamijin_fanta <kamijin@live.jp>
2017-08-04 23:04:18 +0900

    すべてのトランプ列挙した

--- src/blackjack.js
+++ src/blackjack.js
@@ -2,8 +2,19 @@ const Trump = require('./trump.js');
 class BlackJack{
     constructor(){
         console.log(1);
-        let trump = new Trump("Dia", 1);
-        console.log(trump);
+        let trump = new Trump(Trump.MARK.D, 1);
+        let allTump = [];
+
+        for (let m of Trump.ALL_MARK) {    
+            allTump.push(... new Array(13)
+                .fill(0)
+                .map((_, i) => i + 1)
+                .map(v => new Trump(m, v))
+            )
+        }
+
+
+        console.log(trump, allTump);
     }
 }
 module.exports = BlackJack;
--- src/trump.js
+++ src/trump.js
@@ -1,4 +1,12 @@
 class Trump{
+    static get ALL_MARK () {
+        return [
+            Trump.MARK.D,
+            Trump.MARK.C,
+            Trump.MARK.S,
+            Trump.MARK.H,
+        ]
+    }
     static get MARK () {
         return {
             D : 1,
