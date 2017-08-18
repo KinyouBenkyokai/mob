b22b7b57a1c13835a4c24771f7767df5af50c800
avis <b3326@oic.jp>
2017-08-18 08:46:32 -0700

    バースト処理の追加

--- src/player.js
+++ src/player.js
@@ -19,9 +19,14 @@ class Player {
     let power = 0;
     // 複製したデッキを13→1にソートする
     let sorted = [].concat(this.deck).sort((a, b) => b.number - a.number);
+    // Aの枚数を取得する
+    let ANumber = this.deck.filter(_=> _.number===1).length;
     for (let trump of sorted) {
+      // バースト処理
+      if( trump.number === 1 && power < 12 - (ANumber--) ){
+        trump.number = 1;
+      }
       power += trump.number;
-      // todo Aの処理
     }
     return power;
   }
